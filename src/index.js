d9ab6e3d2fa8c163c704f4890324c21743426ba0
Pwako11 <akolo@hotmail.com>
2021-05-17 00:26:22 -0400

    Completed fetch for adding a user

--- src/index.js
+++ src/index.js
@@ -1,1 +1,35 @@
-console.log("hello world")
+function getNewUser() {
+    let newUserForm = document.getElementById("new-user-form")
+    newUserForm.addEventListener("submit", function(event){
+        event.preventDefault()
+        console.log("event", event.target[0].value)
+        submitData(event.target)
+    })
+    newUserForm.reset()
+}
+
+getNewUser()
+
+function submitData(signInForm){
+   
+    let data = {}
+    data.email = signInForm[0].value
+    data.name = signInForm[1].value
+    
+    let configObj = {
+        method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+                'Accept': 'application/json'
+            }, 
+            body: JSON.stringify(data)
+    }
+
+    return fetch("http://localhost:3000/users", configObj)
+    .then(response => response.json())
+    .then(reservation => console.log("reservation", reservation))
+    .catch(function(error) {
+        alert("Please provide an email and user name to make restuarants reservations");
+        console.log(error.message)
+    })
+}
